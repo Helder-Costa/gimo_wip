a6dde750d6dd32fc777284592187b7075a17e0cf
Helder-Costa <54402253+Helder-Costa@users.noreply.github.com>
2019-08-23 10:12:34 +0100

    Add files via upload

--- js/demo.js
+++ js/demo.js
@@ -237,10 +237,13 @@
  */
 {
 
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
   // From http://www.quirksmode.org/js/events_properties.html#position
   // Get the mouse position.
